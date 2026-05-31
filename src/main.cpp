ccf04d19575fdca6c6aae02143406ec2cf8faf86
yemreaycicek <yunus.e.aycicek@gmail.com>
2026-05-31 23:16:47 +0300

    feat(core): integrate startup logs and silence default config warning

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
  * @ Author: yaycicek
  * @ Create Time: 2026-05-27 / 22:18:27
  * @ Modified by: yaycicek
- * @ Modified time: 2026-05-31 / 20:44:23
+ * @ Modified time: 2026-05-31 / 22:55:55
  */
 
 #include <cstdlib>
 #include <string>
 
 #include "utils/io.hpp"
+#include "utils/Logger.hpp"
 
 void printUsage() {
     io::errln("Usage: ./bin/webserv [CONFIGURATION_FILE]...");
@@ -17,7 +18,6 @@ void printUsage() {
 
 std::string parseArguments(const int argc, const char* arg) {
     if (argc == 1) {
-        io::println("No configuration file provided. Using default: conf/default.conf");
         return ("conf/default.conf");
     } else if (argc == 2) {
         return (arg);
@@ -30,5 +30,7 @@ std::string parseArguments(const int argc, const char* arg) {
 int main(int argc, char **argv)
 {
     std::string configFile = parseArguments(argc, argv[1]);
+    LOG_INFO("Webserv process started.");
+    LOG_INFO("Target configuration file: " + configFile);
     return (0);
 }
