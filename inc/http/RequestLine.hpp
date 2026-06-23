e2a1ade67abf82b7381a69a85f1bc8445d5c044d
yemreaycicek <yunus.e.aycicek@gmail.com>
2026-06-23 14:15:19 +0300

    feat(http): implement RequestLine parser

---
+++ inc/http/RequestLine.hpp
@@ -0,0 +1,32 @@
+#ifndef WEBSERV_HTTP_REQUEST_LINE_HPP
+#define WEBSERV_HTTP_REQUEST_LINE_HPP
+
+#include <string>
+
+namespace http {
+    enum Method {
+        GET,
+        POST,
+        DELETE,
+        UNKNOWN
+    };
+
+    class RequestLine {
+        public:
+            RequestLine();
+            RequestLine(const RequestLine& other);
+            RequestLine& operator=(const RequestLine& other);
+            ~RequestLine();
+
+            bool parse(std::string& requestLine);
+    
+        private:
+            Method _method;
+            std::string _uri;
+            std::string _version;
+
+            Method stringToMethod(const std::string& methodString) const;
+    };
+}
+
+#endif // WEBSERV_HTTP_REQUEST_LINE_HPP
---
+++ src/http/RequestLine.cpp
@@ -0,0 +1,64 @@
+#include "http/RequestLine.hpp"
+
+#include <stdexcept>
+#include <string>
+
+namespace http {
+    RequestLine::RequestLine() : _method(UNKNOWN) {}
+    RequestLine::RequestLine(const RequestLine& other) : _method(other._method) {}
+    
+    RequestLine& RequestLine::operator=(const RequestLine& other) {
+        if (this != &other) {
+            _method = other._method;
+            _uri = other._uri;
+            _version = other._version;
+        }
+        return ((*this));
+    }
+    
+    RequestLine::~RequestLine() {}
+
+    bool RequestLine::parse(std::string& buffer) {
+        std::size_t crlf = buffer.find("\r\n");
+        if (crlf == std::string::npos) {
+            return (false);
+        }
+
+        std::string line = buffer.substr(0, crlf);
+        buffer.erase(0, crlf + 2);
+
+        std::size_t firstSpace = line.find(' ');
+        if (firstSpace == std::string::npos) {
+            throw std::runtime_error("400 Bad Request: Malformed request line (missing URI)");
+        }
+        std::size_t secondSpace = line.find(' ', firstSpace + 1);
+        if (secondSpace == std::string::npos) {
+            throw std::runtime_error("400 Bad Request: Malformed request line (missing HTTP version)");
+        }
+        
+        std::string methodString = line.substr(0, firstSpace);
+        _method = stringToMethod(methodString);
+        if (_method == UNKNOWN) {
+            throw std::runtime_error("501 Not Implemented: Unknown HTTP Method '" + methodString + "'");
+        }
+
+        _uri = line.substr(firstSpace + 1, secondSpace - (firstSpace + 1));
+
+        _version = line.substr(secondSpace + 1);
+        if (_version.find(' ') == std::string::npos) {
+            throw std::runtime_error("400 Bad Request: Trailing garbage in request line");
+        }
+        if (_version != "HTTP/1.1" || _version != "HTTP/1.0") {
+            throw std::runtime_error("505 HTTP Version Not Supported: " + _version);
+        }
+
+        return (true);
+    }
+
+    Method RequestLine::stringToMethod(const std::string& methodString) const {
+        if (methodString == "GET") return (GET);
+        if (methodString == "POST") return (POST);
+        if (methodString == "DELETE") return (DELETE);
+        return (UNKNOWN);
+    }
+}
