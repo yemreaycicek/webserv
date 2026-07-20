7a245d758159d700c8449327ce83f823fd048b77
alikosaca <alikosaca0@gmail.com>
2026-07-20 22:35:53 +0300

    fix(Address): update modification timestamp and improve host validation in Address constructor

--- src/net/Address.cpp
+++ src/net/Address.cpp
@@ -2,7 +2,7 @@
  * @ Author: akosaca
  * @ Create Time: 2026-07-20 / 18:37:09
  * @ Modified by: akosaca
- * @ Modified time: 2026-07-20 / 22:04:59
+ * @ Modified time: 2026-07-20 / 22:33:36
  */
 
 #include "net/Address.hpp"
@@ -10,14 +10,21 @@
 #include <string>
 #include <cstring>     // std::memset, std::memcpy
 #include <arpa/inet.h> // htons, ntohs
-#include <stdexcept>   // std::invalid_argument
+#include <stdexcept>
+#include <netinet/in.h>
 
 namespace net {
     Address::Address(const std::string& host, unsigned short port){
         std::memset(&_addr, 0, sizeof(_addr));
         _addr.sin_family = AF_INET;
         _addr.sin_port = htons(port);
-        _addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        if (host == "0.0.0.0"){
+            _addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        } else if (host == "127.0.0.1") {
+            _addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+        } else {
+            throw std::invalid_argument("Address: desteklenmeyen host: " + host); //? burada sadece iki tanesi mi olmalı yoksa hepsini almalıyız?
+        }
     }
 
     const struct sockaddr* Address::getAddr() const {
