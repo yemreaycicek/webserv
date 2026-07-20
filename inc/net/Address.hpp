3a2661833b5c1515a836b0a520b03340a49bda32
alikosaca <alikosaca0@gmail.com>
2026-07-20 21:48:16 +0300

    feat(Address): add Address class for handling network addresses

---
+++ inc/net/Address.hpp
@@ -0,0 +1,27 @@
+/**
+ * @ Author: akosaca
+ * @ Create Time: 2026-07-20 / 18:37:49
+ * @ Modified by: akosaca
+ * @ Modified time: 2026-07-20 / 21:46:42
+ */
+
+#ifndef ADRESS_HPP
+#define ADRESS_HPP
+
+#include <string>
+#include <netinet/in.h>   // struct sockaddr_in
+#include <sys/socket.h>   // struct sockaddr, socklen_t
+
+namespace net {
+    class Address {
+        private:
+            struct sockaddr_in _addr;
+        public:
+            Address(const std::string& host, unsigned short port);
+            const struct sockaddr* getAddr() const;
+            socklen_t              getSize() const;
+            unsigned short         getPort() const;
+    };    
+}
+
+#endif //WEBSERV_NET_ADRESS_HPP
