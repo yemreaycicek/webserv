3a3bfe0faaf2286c1d79398a64afa975678274a9
alikosaca <alikosaca0@gmail.com>
2026-07-22 00:05:05 +0300

    feat(Poller): implement Poller class for managing file descriptor events

---
+++ src/exec/Poller.cpp
@@ -0,0 +1,33 @@
+/**
+ * @ Author: akosaca
+ * @ Create Time: 2026-07-21 / 22:57:06
+ * @ Modified by: akosaca
+ * @ Modified time: 2026-07-22 / 00:04:45
+ */
+
+#include "exec/Poller.hpp"
+
+#include <poll.h>
+#include <vector>
+
+namespace exec {
+    Poller::Poller() {}
+    Poller::~Poller() {}
+
+    void Poller::addFd(int fd, short events){
+        pollfd new_fd;
+        new_fd.fd = fd;
+        new_fd.events = events;
+        new_fd.revents = 0;
+        _fds.push_back(new_fd);
+    }
+
+    void Poller::deleteFd(int fd){
+        for (std::vector<pollfd>::iterator it = _fds.begin(); it != _fds.end(); it++){
+            if (it->fd == fd) {
+                _fds.erase(it);
+                return;
+            }
+        }
+    }
+}
