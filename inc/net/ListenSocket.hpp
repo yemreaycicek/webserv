b9adb4ee61af09b3893f39e1bccae9523efc62d4
alikosaca <alikosaca0@gmail.com>
2026-07-21 17:17:11 +0300

    fix(ListenSocket): update modification timestamp and adjust method names for clarity

--- inc/net/ListenSocket.hpp
+++ inc/net/ListenSocket.hpp
@@ -2,7 +2,7 @@
  * @ Author: akosaca
  * @ Create Time: 2026-07-20 / 18:36:42
  * @ Modified by: akosaca
- * @ Modified time: 2026-07-21 / 16:47:08
+ * @ Modified time: 2026-07-21 / 17:12:55
  */
 
 #ifndef WEBSERV_NET_LISTENSOCKET_HPP
@@ -10,13 +10,23 @@
 
 #include "Address.hpp"
 #include "Socket.hpp"
+#include "net/Exception.hpp"
+#include <stdexcept>
+
 
 namespace net {
     class ListenSocket : public Socket {
         public:
             explicit ListenSocket(const Address& addr);
-            int accept();
-            ~ListenSocket();         
+            int acceptRun();
+            ~ListenSocket();
+            class CreateSocketERR : public Exception {
+                public:
+                    CreateSocketERR(const std::string& message);
+                    ~CreateSocketERR() throw();
+            };
+        private:
+            static int createSocket();
     };
 }
 
