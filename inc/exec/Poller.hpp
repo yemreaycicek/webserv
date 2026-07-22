237c8ef054f7abaa5ccee1b6b1654a58d46b23c5
alikosaca <alikosaca0@gmail.com>
2026-07-22 22:21:56 +0300

    fix(Poller): update modification timestamp and adjust pollReady method signature

--- inc/exec/Poller.hpp
+++ inc/exec/Poller.hpp
@@ -2,11 +2,13 @@
  * @ Author: akosaca
  * @ Create Time: 2026-07-21 / 22:57:26
  * @ Modified by: akosaca
- * @ Modified time: 2026-07-21 / 23:50:14
+ * @ Modified time: 2026-07-22 / 17:04:23
  */
 
 #ifndef WEBSERV_EXEC_POLLER_HPP
 #define WEBSERV_EXEC_POLLER_HPP
+
+#include "exec/Exception.hpp"
 
 #include <poll.h>
 #include <vector>
@@ -19,8 +21,12 @@ namespace exec {
             void                addFd(int fd, short events);
             void                deleteFd(int fd);
             void                setFdEvents(int fd, short events);
-            std::vector<pollfd> pollReady();
-    
+            std::vector<pollfd> pollReady(int timeout);
+            class PollError : public Exception{
+                public:
+                    PollError(const std::string& message);
+                    ~PollError() throw();
+            };
         private:
             std::vector<pollfd> _fds;
     };
