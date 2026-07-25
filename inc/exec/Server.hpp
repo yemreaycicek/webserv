019b75eaf6347d208d1e16cb25505187fd37d981
alikosaca <alikosaca0@gmail.com>
2026-07-25 21:23:26 +0300

    fix(Server): update modification timestamp and add delCl method declaration

--- inc/exec/Server.hpp
+++ inc/exec/Server.hpp
@@ -2,7 +2,7 @@
  * @ Author: akosaca
  * @ Create Time: 2026-07-22 / 20:11:41
  * @ Modified by: akosaca
- * @ Modified time: 2026-07-25 / 20:17:07
+ * @ Modified time: 2026-07-25 / 19:19:09
  */
 
 #ifndef WEBSERV_EXEC_SERVER_HPP
@@ -29,6 +29,7 @@ namespace exec {
             Server&                             operator=(const Server& other);
             
             void                                addCl(int cl_fd, short events);
+            void                                delCl(int fd);
             void                                handleCl(int fd, short revents);
             void                                acceptCl(int ls_fd);
 
