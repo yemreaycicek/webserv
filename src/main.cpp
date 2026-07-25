07e1800174f0aaadc84a9967e12dbad5518b4622
alikosaca <alikosaca0@gmail.com>
2026-07-25 21:24:23 +0300

    fix(Server): update modification timestamp and initialize server in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 /**
  * @ Author: yaycicek
  * @ Create Time: 2026-05-27 / 22:18:27
- * @ Modified by: yaycicek
- * @ Modified time: 2026-07-09 / 14:45:53
+ * @ Modified by: akosaca
+ * @ Modified time: 2026-07-25 / 21:23:58
  */
 
 #include <string>
@@ -12,6 +12,7 @@
 #include "config/Router.hpp"
 #include "utils/arg.hpp"
 #include "utils/io.hpp"
+#include "exec/Server.hpp"
 
 int main(int argc, char **argv)
 {
@@ -21,6 +22,8 @@ int main(int argc, char **argv)
         config::Lexer   lexer;
         config::Parser  parser(lexer.tokenize(args.getConfigFileContent()));
         config::Router  router(parser.parse());
+        exec::Server    server(router);
+        server.run();
     } catch (const config::Exception& e) {
         io::println(std::string("Config Error: ") + e.what());
         return (1);
