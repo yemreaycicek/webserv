7cf3069b428269652fe3b417c43bc362dd82eca8
alikosaca <alikosaca0@gmail.com>
2026-07-25 16:54:22 +0300

    fix(Server): update modification timestamp and improve constructor parameter naming

--- inc/exec/Server.hpp
+++ inc/exec/Server.hpp
@@ -2,7 +2,7 @@
  * @ Author: akosaca
  * @ Create Time: 2026-07-22 / 20:11:41
  * @ Modified by: akosaca
- * @ Modified time: 2026-07-22 / 22:43:34
+ * @ Modified time: 2026-07-25 / 16:51:59
  */
 
 #ifndef WEBSERV_EXEC_SERVER_HPP
@@ -20,17 +20,19 @@
 namespace exec {
     class Server {
         public:
-            Server(const config::Router& a);
+            Server(const config::Router& config);
             ~Server();
-            void    run();
-            
+
+
         private:
             Server(const Server& other);
-            Server& operator=(const Server& other);
-            //void    addClient(int fd);
-            //void    deleteClient(int fd);
-            //std::vector<net::ListenSocket*> _listenSockets;
-            
+            Server&                             operator=(const Server& other);
+            const config::Router&               _config;
+            exec::Poller                        _poller;
+            std::vector<net::ListenSocket*>     _listenSockets;          
+            std::map<int, exec::Connection*>    _connections;
+            std::map<int, std::string>          _clAddr;
+            std::map<int, std::string>          _lsAddr;
     };
 }
 
