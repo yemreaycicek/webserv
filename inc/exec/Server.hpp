8282bf0340791d45fd6618efbca83e707fc2d56b
alikosaca <alikosaca0@gmail.com>
2026-08-20 16:16:37 +0300

    fix(Server): update CGI timeout handling and enhance request processing with new methods

--- inc/exec/Server.hpp
+++ inc/exec/Server.hpp
@@ -2,7 +2,7 @@
  * @ Author: akosaca
  * @ Create Time: 2026-07-22 / 20:11:41
  * @ Modified by: akosaca
- * @ Modified time: 2026-08-14 / 20:38:54
+ * @ Modified time: 2026-08-20 / 16:07:35
  */
 
 #ifndef WEBSERV_EXEC_SERVER_HPP
@@ -21,7 +21,7 @@
 #include <string>
 
 namespace exec {
-    static const int CGI_TIMEOUT_SEC = 5;
+    static const int CGI_TIMEOUT_SEC = 60;
     class Server {
         public:
             Server(const config::Router& config);
@@ -31,14 +31,18 @@ namespace exec {
         private:
             Server(const Server& other);
             Server&                             operator=(const Server& other);
-            
+
             void                                addCl(int cl_fd, short events);
             void                                delCl(int fd);
             void                                delCgi(Cgi* cgi);
             void                                handleCl(int fd, short revents);
             void                                acceptCl(int ls_fd);
-            void                                buildCgi(int fd, CgiInfo& info);
+            void                                buildCgi(int fd, CgiInfo& info, bool bodyComplete);
+            bool                                dispatchCgi(int fd, exec::Connection* conCl);
+            void                                feedCgiStream(int fd, exec::Connection* conCl, Cgi* cgi);
             void                                handleCgi(int fd);
+            void                                relayCgiOutput(Cgi* cgi);
+            std::string                         buildStreamedCgiHead(const std::string& headerBlock) const;
             std::string                         cgiToHttp(const std::string& raw) const;
 
             const config::Router&               _config;
@@ -50,6 +54,7 @@ namespace exec {
             std::map<int, std::string>          _clAddr;
             std::map<int, std::string>          _lsAddr;
             std::map<int, Cgi*>                 _cgi;
+            std::map<int, Cgi*>                 _cgiByClient;
             std::vector<Cgi*>                   _cgiToClose;
     };
 }
--- src/exec/Server.cpp
+++ src/exec/Server.cpp
@@ -2,7 +2,7 @@
  * @ Author: akosaca
  * @ Create Time: 2026-07-22 / 20:11:29
  * @ Modified by: akosaca
- * @ Modified time: 2026-08-15 / 15:20:29
+ * @ Modified time: 2026-08-20 / 16:15:36
  */
 
 #include "exec/Server.hpp"
@@ -67,68 +67,136 @@ namespace exec {
         delete it->second;
         _connections.erase(it);
         _clAddr.erase(fd);
+        _cgiByClient.erase(fd);
     }
-    void Server::buildCgi(int fd, CgiInfo& info) {
+
+    void Server::buildCgi(int fd, CgiInfo& info, bool bodyComplete) {
         Cgi* cgi = new Cgi(fd);
         cgi->run(info.reqData, info.interpreter, info.scriptPath);
         if (cgi->getState() == FAILED) {
             delete cgi;
+            std::map<int, exec::Connection*>::iterator it = _connections.find(fd);
+            if (it != _connections.end()) {
+                it->second->setResponse("HTTP/1.1 502 Bad Gateway\r\nContent-Length: 0\r\nConnection: close\r\n\r\n");
+                _poller.setFdEvents(fd, POLLOUT);
+            }
             return;
         }
+        if (bodyComplete) cgi->finishInput();
         int outFd = cgi->getOutFd();
         _poller.addFd(outFd, POLLIN);
         _cgi[outFd] = cgi;
         if (cgi->getInFd() != -1) {
             int inFd = cgi->getInFd();
-            _poller.addFd(inFd, POLLOUT);
+            short events = (bodyComplete || cgi->pendingInputBytes() > 0) ? POLLOUT : 0;
+            _poller.addFd(inFd, events);
             _cgi[inFd] = cgi;
         }
-        _poller.setFdEvents(fd, 0);
+        if (bodyComplete) _poller.setFdEvents(fd, 0);
+        else _cgiByClient[fd] = cgi;
     }
+
+    bool Server::dispatchCgi(int fd, exec::Connection* conCl) {
+        try {
+            const config::ServerBlock& sb = _config.getServerBlock(_clAddr[fd]);
+            CgiInfo info;
+            std::string errRes;
+            CgiDispatch d = _executor.prepareCgi(sb, conCl->getRequest(), info, errRes);
+            if (d == CGI_NONE) return (false);
+            if (d == CGI_ERROR) {
+                conCl->clearRequestBody();
+                conCl->setResponse(errRes);
+                _poller.setFdEvents(fd, POLLOUT);
+                return (true);
+            }
+            bool bodyComplete = conCl->isRequestComplete();
+            conCl->clearRequestBody();
+            buildCgi(fd, info, bodyComplete);
+            return (true);
+        } catch (const std::exception& e) {
+            conCl->setResponse("HTTP/1.1 500 Internal Server Error\r\nContent-Length: 0\r\nConnection: close\r\n\r\n");
+            _poller.setFdEvents(fd, POLLOUT);
+            return (true);
+        }
+    }
+
+    void Server::feedCgiStream(int fd, exec::Connection* conCl, Cgi* cgi) {
+        std::string chunk = conCl->takeAvailableBody();
+        cgi->feed(chunk);
+        bool complete = conCl->isRequestComplete();
+        if (complete) cgi->finishInput();
+        if (cgi->getInFd() != -1 && (!chunk.empty() || complete)) {
+            _poller.setFdEvents(cgi->getInFd(), POLLOUT);
+        }
+        if (complete) {
+            _cgiByClient.erase(fd);
+            _poller.setFdEvents(fd, 0);
+        }
+    }
+
     void Server::handleCl(int fd, short revents) {
         exec::Connection *conCl = _connections[fd];
         if (revents & POLLIN) {
             conCl->onReadable();
-            if (conCl->isRequestComplete()) {
-                std::string res;
-                try {
-                    CgiInfo cgiInfo;
-                    const config::ServerBlock& sb = _config.getServerBlock(_clAddr[fd]);
-                    res = _executor.execute(sb, conCl->getRequest(), cgiInfo);
-                    if (cgiInfo.isCgi) {
-                        buildCgi(fd, cgiInfo);
+            if (conCl->getState() == CLOSING) {
+                _cgiByClient.erase(fd);
+                _toClose.push_back(fd);
+                return;
+            }
+            std::map<int, Cgi*>::iterator streaming = _cgiByClient.find(fd);
+            if (streaming != _cgiByClient.end()) {
+                feedCgiStream(fd, conCl, streaming->second);
+            }
+            else {
+                bool handled = false;
+                if (conCl->getRequest().isHeadersReady() && !conCl->getRequest().hasError()) handled = dispatchCgi(fd, conCl);
+                if (!handled) {
+                    if (conCl->isRequestComplete()) {
+                        try {
+                            const config::ServerBlock& sb = _config.getServerBlock(_clAddr[fd]);
+                            std::string res = _executor.execute(sb, conCl->getRequest());
+                            conCl->clearRequestBody();
+                            conCl->setResponse(res);
+                            _poller.setFdEvents(fd, POLLOUT);
+                        } catch(const std::exception& e) {
+                            conCl->setResponse("HTTP/1.1 500 Internal Server Error\r\nContent-Length: 0\r\nConnection: close\r\n\r\n");
+                            _poller.setFdEvents(fd, POLLOUT);
+                        }
                     }
-                    else {
+                    else if (conCl->getRequest().hasError()) {
+                        http::status::Code code = conCl->getRequest().getErrorCode();
+                        std::string res = "HTTP/1.1 " + str::to_string(code) + " " + http::status::getReasonPhrase(code) + "\r\n" +
+                        "Content-Length: 0\r\n"
+                        "Connection: close\r\n\r\n";
                         conCl->setResponse(res);
                         _poller.setFdEvents(fd, POLLOUT);
                     }
-                } catch(const std::exception& e) {
-                    res = "HTTP/1.1 500 Internal Server Error\r\n "
-                    "Content-Length: 0\r\n"
-                    "Connection: close\r\n\r\n";
                 }
-            }
-            else if (conCl->getRequest().hasError()) {
-                http::status::Code code = conCl->getRequest().getErrorCode();
-                std::string res = "HTTP/1.1 " + str::to_string(code) + " " + http::status::getReasonPhrase(code) + "\r\n" +
-                "Content-Length: 0\r\n"
-                "Connection: close\r\n\r\n";
-                conCl->setResponse(res);
-                _poller.setFdEvents(fd, POLLOUT);
             }
         }
         if (revents & POLLOUT) {
             conCl->onWritable();
-            if (conCl->getState() == CLOSING){
-                _toClose.push_back(fd);
-            }
+            if (conCl->getState() == CLOSING) _toClose.push_back(fd);
+            else if (!conCl->hasPendingOutput()) _poller.setFdEvents(fd, _cgiByClient.count(fd) ? POLLIN : 0);
         }
     }
 
     void Server::handleCgi(int fd) {
         Cgi* cgi = _cgi[fd];
-        if (fd == cgi->getInFd()) cgi->onWritable();
-        else if (fd == cgi->getOutFd()) cgi->onReadable();
+        bool wasInFd = (fd == cgi->getInFd());
+        if (wasInFd) cgi->onWritable();
+        else if (fd == cgi->getOutFd()) {
+            cgi->onReadable();
+            relayCgiOutput(cgi);
+        }
+        if (wasInFd) {
+            if (cgi->getInFd() == -1) {
+                _poller.deleteFd(fd);
+                _cgi.erase(fd);
+            } else if (cgi->pendingInputBytes() == 0) {
+                _poller.setFdEvents(fd, 0);
+            }
+        }
         if (cgi->getState() == DONE || cgi->getState() == FAILED) {
             std::map<int, Cgi*>::iterator it = _cgi.begin();
             while (it != _cgi.end()) {
@@ -138,8 +206,61 @@ namespace exec {
                     _cgi.erase(toErase);
                 } else ++it;
             }
+            std::map<int, Cgi*>::iterator cit = _cgiByClient.find(cgi->getClientFd());
+            if (cit != _cgiByClient.end() && cit->second == cgi) _cgiByClient.erase(cit);
             _cgiToClose.push_back(cgi);
         }
+    }
+
+    void Server::relayCgiOutput(Cgi* cgi) {
+        std::map<int, Connection*>::iterator cit = _connections.find(cgi->getClientFd());
+        if (cit == _connections.end()) return;
+        Connection* conCl = cit->second;
+
+        if (!cgi->headersRelayed()) {
+            const std::string& raw = cgi->peekOutput();
+            std::string::size_type pos = raw.find("\r\n\r\n");
+            std::size_t sepLen = 4;
+            if (pos == std::string::npos) {
+                pos = raw.find("\n\n");
+                sepLen = 2;
+            }
+            if (pos == std::string::npos) return;
+            std::string head = buildStreamedCgiHead(raw.substr(0, pos));
+            cgi->dropOutputPrefix(pos + sepLen);
+            cgi->setHeadersRelayed();
+            conCl->beginStreamResponse(head);
+            short events = _cgiByClient.count(cgi->getClientFd()) ? (POLLIN | POLLOUT) : POLLOUT;
+            _poller.setFdEvents(cgi->getClientFd(), events);
+        }
+        std::string body = cgi->takeOutput();
+        if (body.empty()) return;
+        conCl->appendStreamChunk(body);
+        short events = _cgiByClient.count(cgi->getClientFd()) ? (POLLIN | POLLOUT) : POLLOUT;
+        _poller.setFdEvents(cgi->getClientFd(), events);
+    }
+
+    std::string Server::buildStreamedCgiHead(const std::string& headerBlock) const {
+        std::string statusLine = "200 OK";
+        std::string outHeaders;
+        std::stringstream hs(headerBlock);
+        std::string line;
+        while (std::getline(hs, line)) {
+            if (!line.empty() && line[line.size() - 1] == '\r')
+                line.erase(line.size() - 1);
+            if (line.empty())
+                continue;
+            std::string lower = str::tolower(line);
+            if (lower.compare(0, 7, "status:") == 0) {
+                std::string val = str::trim(line.substr(7));
+                if (!val.empty()) statusLine = val;
+                continue;
+            }
+            if (lower.compare(0, 15, "content-length:") == 0) continue;
+            if (lower.compare(0, 18, "transfer-encoding:") == 0) continue;
+            outHeaders += line + "\r\n";
+        }
+        return ("HTTP/1.1 " + statusLine + "\r\n" + outHeaders + "Connection: close\r\n\r\n");
     }
 
     std::string Server::cgiToHttp(const std::string& raw) const {
@@ -181,6 +302,7 @@ namespace exec {
         res << "HTTP/1.1 " << statusLine << "\r\n";
         res << outHeaders;
         res << "Content-Length: " << body.size() << "\r\n";
+        res << "Connection: close\r\n";
         res << "\r\n";
         res << body;
         return res.str();
@@ -188,12 +310,18 @@ namespace exec {
 
 
     void Server::delCgi(Cgi* cgi) {
-        std::string res;
-        if (cgi->getState() == DONE) res = cgiToHttp(cgi->rawOutput());
-        else res = "HTTP/1.1 502 Bad Gateway\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
         std::map<int, Connection*>::iterator it = _connections.find(cgi->getClientFd());
         if (it != _connections.end()) {
-            it->second->setResponse(res);
+            if (cgi->headersRelayed()) {
+                std::string remaining = cgi->takeOutput();
+                if (!remaining.empty()) it->second->appendStreamChunk(remaining);
+                it->second->finishStreamResponse();
+            } else {
+                std::string res;
+                if (cgi->getState() == DONE) res = cgiToHttp(cgi->rawOutput());
+                else res = "HTTP/1.1 502 Bad Gateway\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
+                it->second->setResponse(res);
+            }
             _poller.setFdEvents(cgi->getClientFd(), POLLOUT);
         }
         cgi->cleanup();
@@ -204,19 +332,37 @@ namespace exec {
         while (true) {
             std::vector<pollfd> pl = _poller.pollReady(120);
             for (size_t i = 0; i < pl.size(); ++i) {
-                if (_lsAddr.count(pl[i].fd))    acceptCl(pl[i].fd);
-                else if (_cgi.count(pl[i].fd))  handleCgi(pl[i].fd);
-                else                            handleCl(pl[i].fd, pl[i].revents);
+                int fd = pl[i].fd;
+                if (_lsAddr.count(fd))              acceptCl(fd);
+                else if (_cgi.count(fd))            handleCgi(fd);
+                else if (_connections.count(fd))    handleCl(fd, pl[i].revents);
             }
+            std::vector<Cgi*> timedOut;
             for (std::map<int, Cgi*>::const_iterator it = _cgi.begin(); it != _cgi.end(); ++it) {
-                if (it->second->getState() == WRITING || it->second->getState() == READING) {
-                    if (it->second->isTimedOut(CGI_TIMEOUT_SEC)) {
-                        kill(it->second->getPid(), SIGKILL);
-                        it->second->setTimedOut();
-                        _cgiToClose.push_back(it->second);
-
+                Cgi* cgi = it->second;
+                if ((cgi->getState() == WRITING || cgi->getState() == READING) && cgi->isTimedOut(CGI_TIMEOUT_SEC)) {
+                    bool already = false;
+                    for (size_t k = 0; k < timedOut.size(); ++k) {
+                        if (timedOut[k] == cgi) { already = true; break; }
                     }
+                    if (!already) timedOut.push_back(cgi);
                 }
+            }
+            for (size_t i = 0; i < timedOut.size(); ++i) {
+                Cgi* cgi = timedOut[i];
+                kill(cgi->getPid(), SIGKILL);
+                cgi->setTimedOut();
+                std::map<int, Cgi*>::iterator sub = _cgi.begin();
+                while (sub != _cgi.end()) {
+                    if (sub->second == cgi) {
+                        _poller.deleteFd(sub->first);
+                        std::map<int, Cgi*>::iterator toErase = sub++;
+                        _cgi.erase(toErase);
+                    } else ++sub;
+                }
+                std::map<int, Cgi*>::iterator cit = _cgiByClient.find(cgi->getClientFd());
+                if (cit != _cgiByClient.end() && cit->second == cgi) _cgiByClient.erase(cit);
+                _cgiToClose.push_back(cgi);
             }
             for (size_t i = 0; i < _toClose.size(); ++i) {
                 delCl(_toClose[i]);
