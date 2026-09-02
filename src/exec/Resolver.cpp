e03f2aae9b2e8009ad85d846b51727457edf487a
alikosaca <alikosaca0@gmail.com>
2026-09-02 12:49:09 +0300

    fix(Resolver): refactor normalizePath for improved readability

--- src/exec/Resolver.cpp
+++ src/exec/Resolver.cpp
@@ -2,7 +2,7 @@
  * @ Author: akosaca
  * @ Create Time: 2026-07-26 / 00:10:30
  * @ Modified by: akosaca
- * @ Modified time: 2026-08-15 / 13:11:52
+ * @ Modified time: 2026-09-02 / 12:48:09
  */
 
 
@@ -31,39 +31,33 @@ namespace exec {
         return res;
     }
 
-        std::string Resolver::normalizePath(const std::string& path) const {
-            std::vector<std::string> stack;
-            std::stringstream ss(path);
-            std::string segment;
-            while (std::getline(ss, segment, '/')) {
-                if (segment.empty() || segment == ".")
-                    continue;
-                if (segment == "..") {
-                    if (!stack.empty())
-                        stack.pop_back();
-                    continue;
-                }
-                stack.push_back(segment);
+    std::string Resolver::normalizePath(const std::string& path) const {
+        std::vector<std::string> stack;
+        std::stringstream ss(path);
+        std::string segment;
+        while (std::getline(ss, segment, '/')) {
+            if (segment.empty() || segment == ".") continue;
+            if (segment == "..") {
+                if (!stack.empty()) stack.pop_back();
+                continue;
             }
+            stack.push_back(segment);
+        }
 
-            std::string result = "/";
-            for (size_t i = 0; i < stack.size(); ++i) {
-                result += stack[i];
-                if (i + 1 < stack.size())
-                    result += "/";
-            }
-            return (result);
+        std::string result = "/";
+        for (size_t i = 0; i < stack.size(); ++i) {
+            result += stack[i];
+            if (i + 1 < stack.size()) result += "/";
+        }
+        return (result);
     }
 
     std::string Resolver::buildFsPath(const config::LocationBlock& loc, const std::string& uri) const {
         std::string remainder = uri.substr(loc.path.size());
         std::string root = loc.root;
         if (!remainder.empty() && !root.empty()) {
-            if (remainder[0] != '/' && root[root.size() - 1] != '/') {
-                remainder = "/" + remainder;
-            } else if (remainder[0] == '/' && root[root.size() - 1] == '/') {
-                root.erase(root.size() - 1);
-            }
+            if (remainder[0] != '/' && root[root.size() - 1] != '/') remainder = "/" + remainder;
+            else if (remainder[0] == '/' && root[root.size() - 1] == '/') root.erase(root.size() - 1);
         }
         return (root + remainder);
     }
