4dc5d9816a379e6abeea0860165e453841e7ac31
yemreaycicek <yunus.e.aycicek@gmail.com>
2026-09-07 18:51:46 +0300

    fix(io): remove unused functions

--- inc/utils/io.hpp
+++ inc/utils/io.hpp
@@ -2,7 +2,7 @@
  * @ Author: yaycicek
  * @ Create Time: 2026-05-27 / 23:15:18
  * @ Modified by: yaycicek
- * @ Modified time: 2026-06-17 / 14:46:17
+ * @ Modified time: 2026-09-07 / 18:51:12
  */
 
 #ifndef WEBSERV_UTILS_IO_HPP
@@ -11,12 +11,8 @@
 #include <string>
 
 namespace io {
-    void print(const std::string& message);
     void println(const std::string& message);
-    void err(const std::string& message);
     void errln(const std::string& message);
-    void newline();
-    std::string input(const std::string& prompt);
 
     #ifdef DEBUG
         std::string padRight(const std::string& str, std::size_t width);
--- src/utils/io.cpp
+++ src/utils/io.cpp
@@ -2,7 +2,7 @@
  * @ Author: yaycicek
  * @ Create Time: 2026-05-27 / 23:29:02
  * @ Modified by: yaycicek
- * @ Modified time: 2026-06-17 / 14:51:51
+ * @ Modified time: 2026-09-07 / 18:51:01
  */
 
 #include "utils/io.hpp"
@@ -16,34 +16,12 @@
   #include <sstream>
 #endif
 
-void io::print(const std::string& message) {
-    std::cout << message;
-}
-
 void io::println(const std::string& message) {
     std::cout << message << std::endl;
 }
 
-void io::err(const std::string& message) {
-    std::cerr << message;
-}
-
 void io::errln(const std::string& message) {
     std::cerr << message << std::endl;
-}
-
-void io::newline() {
-    std::cout << std::endl;
-}
-
-std::string io::input(const std::string& prompt) {
-    std::string input;
-
-    io::print(prompt);
-    if (!std::getline(std::cin, input)) {
-        throw std::runtime_error("Input stream closed or failed (EOF).");
-    }
-    return (input);
 }
 
 #ifdef DEBUG
